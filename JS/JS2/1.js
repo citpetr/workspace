250a4c504c7d267ad36e40114579494225d0f010
Petr Y <Petr Y>
2016-08-08 01:57:23 +0300

    ;ljk

---
+++ JS/JS2/1.js
@@ -0,0 +1,19 @@
+var test1 = "kulkdjs";
+var test2 = 123;
+var test3 = true;
+var test4 = {};
+var test5 = [];
+var test6 = "food" * 1000;
+var test7 = { "khlkhdfkh": 123 };
+var test8 = ["lkjlkjdskj", 387];
+var test9 = null;
+
+console.log(typeof test1);
+console.log(typeof test2);
+console.log(typeof test3);
+console.log(typeof test4);
+console.log(typeof test5);
+console.log(test6);
+console.log(typeof test7);
+console.log(typeof test8);
+console.log(typeof test9);
