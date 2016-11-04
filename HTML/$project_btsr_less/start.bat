d3a2b8f5b71e39dc4f9780317261a6ae1b62e18a
Petr Y <Petr Y>
2016-11-04 08:00:11 +0300

    smchgs

---
+++ HTML/$project_btsr_less/start.bat
@@ -0,0 +1,3 @@
+start /min koala
+start code
+start browser-sync start --server --files "css/*.*" "js/*.*" "*.*"
---
+++ HTML/test3/run.bat
@@ -0,0 +1,4 @@
+
+start code ./ ./index.html ./css/style.scss ./js/script.js
+start koala
+browser-sync start --server --files "css/*.*" "js/*.*" "*.*"
