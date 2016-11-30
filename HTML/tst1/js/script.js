8c78510adb314a23e4f9fab2aceca61e72db021f
Petr Y <Petr Y>
2016-11-30 19:36:00 +0300

    smchngs

--- HTML/tst1/js/script.js
+++ HTML/tst1/js/script.js
@@ -4,4 +4,20 @@ var redOff = 'offred';
 var greenOn = 'ongreen';
 var greenOff = 'offgreen';
 
-console.log(requestAddr + redOff);
+
+var x = document.getElementId("redon");
+var x1 = document.getElementId("redoff");
+var x2 = document.getElementId("greenon");
+var x3 = document.getElementId("greenoff");
+var y = document.getElementById("rofft");
+
+y.onmouseover = function() {
+    y.innerText = 'HFLKDF';
+}
+
+x.onclick = function() {
+    document.writeln(x);
+    document.writeln(x1);
+    document.writeln(x2);
+    document.writeln(x3);
+}
--- HTML/tst1/run.bat
+++ HTML/tst1/run.bat
@@ -1,3 +1,3 @@
-start code ./index.html ./css/style.scss ./js/script.js
+start code ./ ./index.html ./css/style.scss ./js/script.js
 start koala
 start browser-sync start --server --files "css/*.*" "js/*.*" "*.*"
