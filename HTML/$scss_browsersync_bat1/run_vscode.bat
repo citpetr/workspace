f6050294de6bceec2c17eb94e261eb2a0cb98499
citpetr <cit.petr@gmail.com>
2016-12-09 04:21:09 +0300

    del tst3

--- HTML/$scss_browsersync_bat1/run_vscode.bat
+++
@@ -1,3 +0,0 @@
-start code ./ ./index.html ./css/style.scss ./js/script.js 
-start koala
-start browser-sync start --server --files "css/*.*" "js/*.*" "html/*.html" "*.*"
--- HTML/test3/run.bat
+++
@@ -1,4 +0,0 @@
-
-start code ./ ./index.html ./css/style.scss ./js/script.js
-start koala
-browser-sync start --server --files "css/*.*" "js/*.*" "*.*"
