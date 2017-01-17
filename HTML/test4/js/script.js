e08600dd08f815a5735f59b8998330340c1a3702
Petr Y <Petr Y>
2017-01-17 22:11:12 +0300

    some changes

---
+++ HTML/test4/js/script.js
@@ -0,0 +1,17 @@
+function validateEmail(email) {
+    var mailSeparated = email.split('@');
+    document.writeln(mailSeparated + '<br>');
+
+    if (email.indexOf('@') === -1) {
+        document.writeln('Sorry, but you input is not corrected email. Please try again.' + '<br>');
+    }
+    if (mailSeparated[1].indexOf('.') === -1) {
+        document.writeln('Sorry, but you input is not corrected email. Please try again.' + '<br>');
+    }
+}
+var email1 = "johnny1@yandex.ru";
+var email2 = "hohoho.net";
+
+
+validateEmail(email1);
+validateEmail(email2);
