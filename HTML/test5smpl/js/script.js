29e3addd376fef03ef8760ad51704a94c4f18114
citpetr <cit.petr@gmail.com>
2017-01-18 01:04:46 +0300

    somchngs

--- HTML/test5smpl/js/script.js
+++ HTML/test5smpl/js/script.js
@@ -1,13 +1,3 @@
-var a = 5;
-var b = '5';
+var access = document.getElementById('code1');
+access.innerHTML = 'ggg <br> ggg <br> ggg <br> ggg';
 
-
-document.writeln(a + b);
-document.writeln(typeof(a + b) + '<br>');
-document.writeln(b + a);
-document.writeln(typeof(b + a));
-
-function littlemagic() {
-    document.getElementById("ho").style.color = 'red';
-    document.getElementById("ho").style.border = 'solid 1px gray';
-}
