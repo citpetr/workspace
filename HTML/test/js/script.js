0b10eee1f397274ad2cb090a44b45a78d0b8713d
Petr Y <cit.petr@gmail.com>
2017-10-07 05:31:53 +0300

    smchngs

--- HTML/test/js/script.js
+++ HTML/test/js/script.js
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
-console.log("TEST");
-var a = 0;
-var b = 0;
-
-if (a && b) {
-    console.log("It's a miracle...")
-} else {
-    console.log("Ooops...");
-}
-=======
 document.write("HELLO WORLD!!!");
->>>>>>> 9511bc203e9d45a7563dafd6ba37f43e2357f846
