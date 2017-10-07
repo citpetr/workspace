10bed3b2ee16e5f6379eb555e26b161df6e69bb0
Petr Y <cit.petr@gmail.com>
2017-10-07 05:28:11 +0300

    somchngs

--- HTML/test/js/script.js
+++ HTML/test/js/script.js
@@ -1,5 +1,9 @@
-window.onload = action();
+console.log("TEST");
+var a = 0;
+var b = 0;
 
-function action() {
-
+if (a && b) {
+    console.log("It's a miracle...")
+} else {
+    console.log("Ooops...");
 }
